f92563bac5c5f16acab882cbdbc41b3073c11480
Neel Awsare <99767480+awsare@users.noreply.github.com>
2022-06-24 20:26:04 +0000

    More in-depth responses and a daily word call

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ A simple API for wordle clones.
 # Calls
 - `/get/` returns a random five letter word from the Wordle word list.
 - `/ask/?word={word}` returns true or false if the word is a valid five letter word.
+- `/daily/?day={day}` returns a word based on the day given (1 to 2315).
--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-import json, random, words
+import json, random, words, time
 from flask import *
 
 app = Flask(__name__)
 
 @app.route('/get/', methods=['GET'])
 def get():
-	return json.dumps({'Get': random.choice(words.targetWords)})
+	return json.dumps({'Call' : 'Get', 'Status' : 200, 'Response' : random.choice(words.targetWords), 'Timestamp' : time.time()})
 
 @app.route('/ask/', methods=['GET'])
 def ask():
 	word = str(request.args.get('word'))
-	return json.dumps({'Ask': word in words.dictionary})
+	if word != "None":
+		return json.dumps({'Call' : 'Ask', 'Status' : 200, 'Response': word in words.dictionary, 'Timestamp' : time.time()})
+	return json.dumps({'Call': 'Ask', 'Status' : 400, 'Details' : 'No word provided', 'Timestamp' : time.time()})
+
+@app.route('/daily/', methods=['GET'])
+def daily():
+	day = request.args.get('day')
+	if day:
+		day = int(day)
+		if (day > 0 and day < len(words.targetWords)):
+			return json.dumps({'Call' : 'Daily', 'Status' : 200, 'Response': words.targetWords[day - 1], 'Timestamp' : time.time()})
+		return json.dumps({'Call': 'Daily', 'Status' : 400, 'Details' : 'Day must be between 1 and ' + str(len(words.targetWords)), 'Timestamp' : time.time()})
+	return json.dumps({'Call': 'Daily', 'Status' : 400, 'Details' : 'No day provided', 'Timestamp' : time.time()})
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=81)
