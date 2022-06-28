ea4ec298cdbbede91358444d4ad43c9ff8f830fa
Neel Awsare <99767480+awsare@users.noreply.github.com>
2022-06-28 11:27:04 -0400

    Code organization and more error catches

--- main.py
+++ main.py
@@ -1,28 +1,83 @@
-import json, random, words, time
+import random, words, time
 from flask import *
 
 app = Flask(__name__)
 
 @app.route('/get/', methods=['GET'])
 def get():
-	return json.dumps({'Call' : 'Get', 'Status' : 200, 'Response' : random.choice(words.targetWords), 'Timestamp' : time.time()})
+	return {
+		'Call' : 'Get',
+		'Status' : 200,
+		'Response' : random.choice(words.targetWords),
+		'Timestamp' : time.time()
+	}, 200
 
 @app.route('/ask/', methods=['GET'])
 def ask():
 	word = str(request.args.get('word'))
-	if word != "None":
-		return json.dumps({'Call' : 'Ask', 'Status' : 200, 'Response': word in words.dictionary, 'Timestamp' : time.time()})
-	return json.dumps({'Call': 'Ask', 'Status' : 400, 'Details' : 'No word provided', 'Timestamp' : time.time()})
+	
+	if word == "None":
+		return {
+			'Call': 'Ask',
+			'Status' : 400,
+			'Details' : 'No word provided',
+			'Timestamp' : time.time()
+		}, 400
+		
+	if len(word) != 5:
+		return {
+			'Call': 'Ask',
+			'Status' : 400,
+			'Details' : 'Word must be five letters',
+			'Timestamp' : time.time()
+		}, 400
+		
+	return {
+		'Call' : 'Ask',
+		'Status' : 200,
+		'Response' : word in words.dictionary,
+		'Timestamp' : time.time()
+	}, 200
 
 @app.route('/daily/', methods=['GET'])
 def daily():
 	day = request.args.get('day')
-	if day:
+	if not day:
+		return {
+			'Call': 'Daily',
+			'Status' : 400,
+			'Details' : 'No day provided',
+			'Timestamp' : time.time()
+		}, 400
+	
+	try:
 		day = int(day)
-		if (day > 0 and day < len(words.targetWords)):
-			return json.dumps({'Call' : 'Daily', 'Status' : 200, 'Response': words.targetWords[day - 1], 'Timestamp' : time.time()})
-		return json.dumps({'Call': 'Daily', 'Status' : 400, 'Details' : 'Day must be between 1 and ' + str(len(words.targetWords)), 'Timestamp' : time.time()})
-	return json.dumps({'Call': 'Daily', 'Status' : 400, 'Details' : 'No day provided', 'Timestamp' : time.time()})
+	except ValueError:
+		return {
+			'Call': 'Daily',
+			'Status' : 400,
+			'Details' : 'Day must be an integer between 1 and ' + str(len(words.targetWords)),
+			'Timestamp' : time.time()
+		}, 400
+		
+	if (day > 0 and day < len(words.targetWords)):
+		return {
+			'Call': 'Daily',
+			'Status' : 400,
+			'Details' : 'Day must be an integer between 1 and ' + str(len(words.targetWords)),
+			'Timestamp' : time.time()
+		}, 400
+		
+	return {
+		'Call' : 'Daily',
+		'Status' : 200,
+		'Response' : words.targetWords[day - 1],
+		'Timestamp' : time.time()
+	}, 200
+
+@app.errorhandler(404)
+def notFound(error):
+    return "Page not found", 404
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=81)
